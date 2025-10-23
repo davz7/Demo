42b6605b48d2f224a9aa81c9b0c91c0d4a59d264
davz7 <sadrackdavor@gmail.com>
2025-10-23 16:45:27 -0600

    feat: Creación de pantalla de recuperación de contraseña y componentes de UI
    Se ha creado la estructura inicial para la pantalla de recuperación de contraseña.
    
    Cambios específicos:
    - Se actualizó el diseño y los estilos de `PrimaryButton` y `CancelButton`, ajustando colores, bordes y modificadores para ocupar el ancho completo.
    - Se ha añadido el componente reutilizable `InputTextField` para campos de texto con un diseño `Outlined`.
    - Se introdujo un `TextComponent` genérico para simplificar la creación de textos en la interfaz.
    - Se agregó el modelo de datos `UserData` para gestionar la información del usuario, como el correo electrónico y el código de verificación.

---
+++ app/src/main/java/mx/edu/utez/demo/data/model/UserData.kt
@@ -0,0 +1,6 @@
+package mx.edu.utez.demo.data.model
+
+data class UserData(
+    val email: String,
+    val verificationCode: String
+)
--- app/src/main/java/mx/edu/utez/demo/ui/components/buttons/PrimaryButton.kt
+++ app/src/main/java/mx/edu/utez/demo/ui/components/buttons/PrimaryButton.kt
@@ -1,4 +1,9 @@
-package mx.edu.utez.demo.ui.components.buttonsimport androidx.compose.material3.*
+package mx.edu.utez.demo.ui.components.buttons
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -6,19 +11,16 @@ import androidx.compose.ui.unit.dp
 
 @Composable
 fun PrimaryButton(
+    onClick: () -> Unit,
     text: String,
-    onClick: () -> Unit
+    modifier: Modifier = Modifier
 ) {
     Button(
         onClick = onClick,
-        modifier = Modifier,
-        shape = MaterialTheme.shapes.medium,
-        elevation = ButtonDefaults.buttonElevation(4.dp),
-        colors = ButtonDefaults.buttonColors(
-            containerColor = Color(0xFF2196F3)
-        )
-
+        modifier = modifier.fillMaxWidth(),
+        shape = androidx.compose.foundation.shape.RoundedCornerShape(8.dp),
+        colors = ButtonDefaults.buttonColors(containerColor  = Color(0xFF6200EE))
     ) {
-        Text(text = text, style = MaterialTheme.typography.bodyLarge)
+        Text(text = text, color = Color.White)
     }
 }
