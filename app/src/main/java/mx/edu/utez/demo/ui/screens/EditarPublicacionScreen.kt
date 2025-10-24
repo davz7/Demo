c2ce8150a4b8fa35d971415a5c09e339d89ca565
davz7 <sadrackdavor@gmail.com>
2025-10-23 22:02:18 -0600

    feat: Adición de dependencias y actualización de pantalla de edición
    Se han añadido nuevas dependencias para extender las funcionalidades de la interfaz de usuario y se realizó una pequeña actualización en la pantalla de edición de publicaciones.
    
    Cambios específicos:
    - Se agregó la dependencia `material-icons-extended` para tener acceso a un conjunto más amplio de iconos en Compose.
    - Se incorporó la dependencia `vision-internal-vkp` de ML Kit, versión 18.2.3, en `libs.versions.toml` y `app/build.gradle.kts`.
    - Se añadió un import no utilizado (`com.google.android.libraries.intelligence.acceleration.Analytics`) en `EditarPublicacionScreen.kt`.

--- app/src/main/java/mx/edu/utez/demo/ui/screens/EditarPublicacionScreen.kt
+++ app/src/main/java/mx/edu/utez/demo/ui/screens/EditarPublicacionScreen.kt
@@ -23,6 +23,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.google.android.libraries.intelligence.acceleration.Analytics
 import mx.edu.utez.demo.R
 
 @Composable
